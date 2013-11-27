bb327afa1a3d0660193ae3b7c90b7ddfb00fb821
unknown <Raissa@Raissa-HP.(none)>
2013-11-27 15:30:38 -0200

    frontend galeria dualista ok

--- frontend/galeria-dualista.php
+++ frontend/galeria-dualista.php
@@ -1,5 +1,95 @@
 <?php get_header() ?>
 
+<div id="galeria-historias" class="container">
 
+		<header>
+
+			<div class="row-fluid">
+
+				<div class="span12">
+
+					<div class="row-fluid">
+						<h1 class="logo span12">Sentidos da Luz</h1>  
+					</div>
+
+					<div class="row-fluid">
+						<div class="wrap-voltar span12">
+							<span>São Paulo</span> > <a href="#">Voltar para o mapa</a>
+						</div>
+					</div>
+
+					<div class="row-fluid">
+
+						<nav class="menu-galerias span12">
+
+							<ul>
+
+								<li><a href="#">Galeria de Histórias</a></li>
+								<li><a href="#">Galeria Dualista</a></li>
+
+							</ul>
+
+						</nav>
+
+					</div>
+
+				</div>
+
+			</div>
+
+		</header>
+
+		<div class="row-fluid">
+
+			<div class="span12">
+
+				<div class="row-fluid">
+
+					<div id="title-wrap-dualista" class="span12">
+
+						<h2>Estação da Luz</h2>
+
+					</div>
+
+				</div>
+
+
+				<div class="row-fluid">
+
+					<div id="wrap-dualista" class="span12">
+
+						<div id="seta-esquerda" class="span1">
+							<img src="<?php bloginfo('template_url') ?>/img/sentidosdaluz-seta-esquerda.png">
+						</div>
+
+						<div class="span10">
+							<figure>
+								<img src="<?php bloginfo('template_url') ?>/img/sentidosdaluz-img-dia.jpg">
+								<img src="<?php bloginfo('template_url') ?>/img/sentidosdaluz-img-noite.jpg">
+
+								<figcaption class="wrap-texto"> Lorem ipsum dolor sit amet, consectetuer adipiscing elit. 
+									Phasellus hendrerit. Pellentesque aliquet nibh nec urna. In nisi neque, 
+									aliquet vel, dapibus id, mattis vel, nisi. Sed pretium, ligula 
+									sollicitudin laoreet viverra, tortor libero sodales leo, eget blandit 
+									nunc tortor eu nibh. Nullam mollis. Ut justo. Suspendisse potenti.
+								</figcaption>
+							</figure>
+						</div>
+
+						<div id="seta-direita" class="span1">
+							<img src="<?php bloginfo('template_url') ?>/img/sentidosdaluz-seta-direita.png">
+						</div>
+						
+					</div>
+
+				</div>
+
+
+			</div>
+
+
+		</div>
+
+	</div>
 
 <?php get_footer() ?>
