f7e59f23bcc9c00a9eb5d168eb85ffac979a4915
Raul Vinicius <raul.portifolio@gmail.com>
2013-11-27 15:29:46 -0200

    backend galeria de historias ok

---
+++ 404.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * The template for displaying 404 pages (Not Found)
+ *
+ * @package WordPress
+ * @subpackage Twenty_Thirteen
+ * @since Twenty Thirteen 1.0
+ */
+
+get_header(); ?>
+
+<h1 class="logo">404</h1>
+
+<?php get_footer(); ?>
